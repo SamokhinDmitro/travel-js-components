33738e43c1ad1e6e191b1124104b194bae9f7268
Samokhin Dmitro <samokhin.dmitriy.dev@gmail.com>
2020-02-01 20:43:34 +0200

    Add server file

---
+++ app.js
@@ -0,0 +1,72 @@
+/*
+Server for my travel site with JS Easy popular components
+ */
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let request = require('request');
+let mysql = require('mysql');
+let app = express();
+
+const PORT = process.env.PORT || 3000;
+
+let connection = mysql.createPool({
+    host: 'remotemysql.com',
+    user: 'rjafWD8Va9',
+    password: 'XXcSytTbHW',
+    database: 'rjafWD8Va9'
+});
+
+//Шаблонизатор
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+//Загрузка главной страницы index
+app.get('/', function(req, res) {
+
+    connection.query('SELECT tabs.title, tabs.subtitle, tabs.text, tabs.photo_id, photos.id, photos.path, photos.exp \n' +
+        '\tFROM tabs\n' +
+        '    INNER JOIN photos\n' +
+        '    ON tabs.photo_id = photos.id', function (err, rows) {
+        if (err) throw new Error;
+
+        res.render('index', {data: rows, title: 'Мой заголовок тут'});
+    });
+});
+
+app.get('/about', function(req,res){
+   res.render('about', {title: 'О нас'});
+});
+
+app.post('/phones', function(req,res){
+    let data = req.body;
+    console.log(data);
+
+    connection.query('INSERT INTO phones SET ?', [data],  function(err,rows){
+        if (err) throw new Error;
+        res.redirect('/');
+    });
+});
+
+app.post('/contacts', function(req,res){
+   let data = req.body;
+console.log(data);
+
+   connection.query('INSERT INTO usersinfo SET ?', [data],  function(err,rows){
+       if (err) throw new Error;
+       res.redirect('/');
+   });
+});
+
+app.use(function(req, res, next){
+    res.status(404).send('Нет такой страинцы повторите ввод!');
+});
+
+app.listen(PORT, function(){
+    console.log(`Прослушиваем порт по адрессу`);
+});
+
